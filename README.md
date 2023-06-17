26029a79e8fe918110981ccc8254c4df1ef3b440
Igor Ho <igorho2000@gmail.com>
2023-06-17 14:08:09 +0800

    Initial Configs

---
+++ README.md
@@ -0,0 +1,1 @@
+# TUIC Dashboard Documentation
---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { onMounted } from 'vue';
+
+onMounted(() => {
+  document.title = '城市聯合儀表板文件'
+})
+</script>
+
+<template>
+  <div>
+    Documentation!
+  </div>
+</template>
+
+<style scoped></style>
