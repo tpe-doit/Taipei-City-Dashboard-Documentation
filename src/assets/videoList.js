0178193bea1c83dc5033d55336e72b45009f3d8d
Igor Ho <igorho2000@gmail.com>
2024-03-08 10:41:48 +0800

    Added Tutorial Video Support

---
+++ src/assets/videoList.js
@@ -0,0 +1,162 @@
+export const videoList = {
+	introduction: [
+		{
+			titleEn: "Tutorial Overview",
+			titleCh: "課程概要和注意事項",
+			url: "",
+		},
+		{
+			titleEn: "Introduction",
+			titleCh: "儀表板簡介",
+			url: "",
+		},
+	],
+	prerequisites: [
+		{
+			titleEn: "Tech Stack",
+			titleCh: "儀表板技術塔",
+			url: "",
+		},
+		{
+			titleEn: "Project Setup (1)",
+			titleCh: "下載並設定儀表板一",
+			url: "",
+		},
+	],
+	"project-setup": [
+		{
+			titleEn: "Project Setup (2)",
+			titleCh: "下載並設定儀表板二",
+			url: "",
+		},
+		{
+			titleEn: "Project Setup (3)",
+			titleCh: "下載並設定儀表板三",
+			url: "",
+		},
+	],
+	"introduction-to-components": [
+		{
+			titleEn: "Introduction to Components",
+			titleCh: "組件概述",
+			url: "",
+		},
+		{
+			titleEn: "Component Config",
+			titleCh: "組件設定",
+			url: "",
+		},
+	],
+	"chart-data": [
+		{
+			titleEn: "Chart and History Data",
+			titleCh: "圖表與歷史資料格式",
+			url: "",
+		},
+	],
+	"supported-chart-types": [
+		{
+			titleEn: "Chart Config",
+			titleCh: "組件圖表設定",
+			url: "",
+		},
+	],
+	"map-data": [
+		{
+			titleEn: "Map Data",
+			titleCh: "地圖資料格式",
+			url: "",
+		},
+	],
+	"supported-map-types": [
+		{
+			titleEn: "Map Config",
+			titleCh: "組件地圖設定",
+			url: "",
+		},
+	],
+	"map-filtering": [
+		{
+			titleEn: "Map Filter Config",
+			titleCh: "組件地圖篩選設定",
+			url: "",
+		},
+	],
+	"history-data": [
+		{
+			titleEn: "History Config",
+			titleCh: "組件歷史軸設定",
+			url: "",
+		},
+	],
+	"customization-overview": [
+		{
+			titleEn: "Create a New Component",
+			titleCh: "建立一個新組件",
+			url: "",
+		},
+		{
+			titleEn: "FE Customization",
+			titleCh: "前端客製化指南",
+			url: "",
+		},
+	],
+	"contribution-overview": [
+		{
+			titleEn: "Create a New Component",
+			titleCh: "建立一個新組件",
+			url: "",
+		},
+		{
+			titleEn: "Contribution Guide",
+			titleCh: "貢獻至官方程式庫",
+			url: "",
+		},
+		{
+			titleEn: "BE Customization",
+			titleCh: "後端客製化指南",
+			url: "",
+		},
+	],
+	"design-guide": [
+		{
+			titleEn: "Design Guide",
+			titleCh: "視覺美學建議",
+			url: "",
+		},
+	],
+	"components-db": [
+		{
+			titleEn: "Introduction to Components",
+			titleCh: "組件概述",
+			url: "",
+		},
+		{
+			titleEn: "Chart Config",
+			titleCh: "組件圖表設定",
+			url: "",
+		},
+		{
+			titleEn: "Map Config",
+			titleCh: "組件地圖設定",
+			url: "",
+		},
+		{
+			titleEn: "Map Filter Config",
+			titleCh: "組件地圖篩選設定",
+			url: "",
+		},
+		{
+			titleEn: "History Config",
+			titleCh: "組件歷史軸設定",
+			url: "",
+		},
+	],
+	"component-data-apis": [
+		{
+			titleEn: "Chart and History Data",
+			titleCh: "圖表與歷史資料格式",
+			url: "",
+		},
+	],
+};
